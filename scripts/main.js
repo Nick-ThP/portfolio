2edc61cb0ef7ee467a27e5410b3851fb3cc0a7aa
Nicklas <nicklas_tp@hotmail.com>
2021-12-06 12:36:41 +0100

    kig på js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
-/* Script til fade effekt på header */
+/* Fade script */
+
 
 const header = document.getElementById('header');
 
 const fadeOutOnScroll = element => {
+
 	if (!element) {
 		return;
 	}
@@ -10,7 +12,6 @@ const fadeOutOnScroll = element => {
 	let distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
 	let elementHeight = element.offsetHeight;
 	let scrollTop = document.documentElement.scrollTop;
-	
 	let opacity = 1;
 	
 	if (scrollTop > distanceToTop) {
@@ -28,3 +29,36 @@ const scrollHandler = () => {
 }
 
 window.addEventListener('scroll', scrollHandler);
+
+
+/* Toggle script */
+
+
+const toggle = (button, article) => {
+
+	let array = [1, 2, 3, 4];
+
+	let x = document.getElementById(button);
+	let y = document.getElementById(article);
+
+	for (i = 0; array.length > 0; i++) {
+
+		if (x.filter(a => typeof a === "number") === array[i]) {
+			x.style.backgroundColor = "gray";
+			y.style.display = "block";
+		} 
+		
+		else {
+			x.style.backgroundColor = "darkgray";
+			y.style.display = "none";
+		}
+
+	}
+
+}
+
+const toggleHandler = () => {
+	toggle(button, article);
+}
+
+document.getElementsByClassName('nav-btn').addEventListener('click', toggleHandler);
