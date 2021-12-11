2679cc79575bd13de5a41c9769d0ab897eacb759
Nicklas <nicklas_tp@hotmail.com>
2021-12-11 17:24:51 +0100

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -57,7 +57,7 @@ const hide = () => {
         let a = sectionArray[i];
 		let b = buttonArray[i];
         let section = document.getElementById(a);
-		let button = document.getElementById(b)
+		let button = document.getElementById(b);
 
         if (visibleA === a && visibleB === b) {
         	section.style.display = "block";
@@ -79,21 +79,18 @@ window.addEventListener("DOMContentLoaded", toggleHandler);
 
 /* Project overlays */
 
-let on = (a, b) => {
+const on = (a, b) => {
 	document.getElementById(a).style.display = "grid";
 	document.getElementById(b).style.display = "none";
 }
 
-let off = (a, b) => {
+const off = (a, b) => {
 	document.getElementById(a).style.display = "none";
 	document.getElementById(b).style.display = "grid";
 }
 
-let allOff = () => {
+const allOff = () => {
 	document.getElementById('overlay1').style.display = "none";
-
-	/* Remember to add new projects */
-
 	document.getElementById('article3').style.display = "grid";
 }
 
