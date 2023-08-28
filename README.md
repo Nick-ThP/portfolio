febfc4cfc1ce5972778412add46ec07a53efe548
Nicklas <95386272+Nick-ThP@users.noreply.github.com>
2023-08-28 21:56:12 +0200

    formatting and border fix

--- README.md
+++ README.md
@@ -3,18 +3,21 @@
 Hello
 
 This project is my personal portfolio.
-	
+
 ## Technologies
+
 Project is created with vanilla:
-* HTML
-* CSS
-* JS
+
+-   HTML
+-   CSS
+-   JS
 
 ## Note
+
 This project was made as a part of the Codecademy frontend engineering course.
 
 https://www.codecademy.com/learn/paths/front-end-engineer-career-path
 
-It is *intended* to be made without frameworks/libraries, utilizing only vanilla coding.
+It is _intended_ to be made without frameworks/libraries, utilizing only vanilla coding.
 
 Thank you for reading.
--- scripts/main.js
+++ scripts/main.js
@@ -1,199 +1,198 @@
 /* To top on refresh */
 
-window.scrollTo(0, 0);
+window.scrollTo(0, 0)
 
 /* To top (mobile) */
 
 const toTop = () => {
 	if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-		window.scroll(0, 0);
+		window.scroll(0, 0)
 	} else {
-		null;
+		null
 	}
-};
+}
 
 /* Fade script */
 
-const header = document.getElementById('header');
+const header = document.getElementById('header')
 
-const fadeOutOnScroll = element => {
-
+const fadeOutOnScroll = (element) => {
 	if (!element) {
-		return;
+		return
 	}
 
-	let distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-	let elementHeight = element.offsetHeight;
-	let scrollTop = document.documentElement.scrollTop;
-	let opacity = 1;
+	let distanceToTop = window.pageYOffset + element.getBoundingClientRect().top
+	let elementHeight = element.offsetHeight
+	let scrollTop = document.documentElement.scrollTop
+	let opacity = 1
 
 	if (scrollTop > distanceToTop) {
-		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+		opacity = 1 - (scrollTop - distanceToTop) / elementHeight
 	}
 
 	if (opacity >= 0) {
-		element.style.opacity = opacity;
+		element.style.opacity = opacity
 	}
 
 	/* Force hide */
 
 	if (opacity < 0.001) {
-		element.style.visibility = 'hidden';
+		element.style.visibility = 'hidden'
 	} else {
-		element.style.visibility = 'visible';
+		element.style.visibility = 'visible'
 	}
-
-};
+}
 
 /* Execution of fade script*/
 
 const scrollHandler = () => {
-	fadeOutOnScroll(header);
-};
+	fadeOutOnScroll(header)
+}
 
-window.addEventListener('scroll', scrollHandler);
+window.addEventListener('scroll', scrollHandler)
 
 /* Toggle script */
 
-const sectionArray = ["section1", "section2", "section3", "section4"];
-const buttonArray = ["button1", "button2", "button3", "button4"];
+const sectionArray = ['section1', 'section2', 'section3', 'section4']
+const buttonArray = ['button1', 'button2', 'button3', 'button4']
 
-let visibleA = null;
-let visibleB = null;
+let visibleA = null
+let visibleB = null
 
 const show = (a, b) => {
-
 	if (visibleA !== a) {
-		visibleA = a;
+		visibleA = a
 	}
-	hide();
+	hide()
 
 	if (visibleB !== b) {
-		visibleB = b;
+		visibleB = b
 	}
-	hide();
+	hide()
 
-	toTop();
-};
+	toTop()
+}
 
 const hide = () => {
 	for (let i = 0; i < sectionArray.length; i++) {
-
-		let a = sectionArray[i];
-		let b = buttonArray[i];
-		let section = document.getElementById(a);
-		let button = document.getElementById(b);
+		let a = sectionArray[i]
+		let b = buttonArray[i]
+		let section = document.getElementById(a)
+		let button = document.getElementById(b)
 
 		if (visibleA === a && visibleB === b) {
-			section.style.display = "block";
-			button.style.backgroundColor = "var(--interface-dark-gray";
-			button.style.pointerEvents = 'none';
+			section.style.display = 'block'
+			button.style.backgroundColor = 'var(--interface-dark-gray'
+			button.style.pointerEvents = 'none'
 		} else {
-			section.style.display = "none";
-			button.style.backgroundColor = "var(--interface-light-gray";
-			button.style.pointerEvents = 'auto';
+			section.style.display = 'none'
+			button.style.backgroundColor = 'var(--interface-light-gray'
+			button.style.pointerEvents = 'auto'
 		}
 	}
 
-	toTop();
-};
+	toTop()
+}
 
 /* Execution of toggle script */
 
 const toggleHandler = () => {
-	show('section1', 'button1');
-};
+	show('section1', 'button1')
+}
 
-window.addEventListener("DOMContentLoaded", toggleHandler);
+window.addEventListener('DOMContentLoaded', toggleHandler)
 
 /* Project overlays */
 
 const on = (a, b) => {
-	document.getElementById(a).style.display = "grid";
-	document.getElementById(b).style.display = "none";
+	document.getElementById(a).style.display = 'grid'
+	document.getElementById(b).style.display = 'none'
 
-	toTop();
-};
+	toTop()
+}
 
 const off = (a, b) => {
-	document.getElementById(a).style.display = "none";
-	document.getElementById(b).style.display = "grid";
+	document.getElementById(a).style.display = 'none'
+	document.getElementById(b).style.display = 'grid'
 
-	toTop();
-};
+	toTop()
+}
 
 const allOff = () => {
-	document.getElementById('overlay1').style.display = "none";
-	document.getElementById('overlay2').style.display = "none";
-	document.getElementById('overlay3').style.display = "none";
-	document.getElementById('overlay4').style.display = "none";
-	document.getElementById('article3').style.display = "grid";
+	document.getElementById('overlay1').style.display = 'none'
+	document.getElementById('overlay2').style.display = 'none'
+	document.getElementById('overlay3').style.display = 'none'
+	document.getElementById('overlay4').style.display = 'none'
+	document.getElementById('article3').style.display = 'grid'
 
-	toTop();
-};
+	toTop()
+}
 
-document.getElementById('button3').addEventListener('click', allOff);
+document.getElementById('button3').addEventListener('click', allOff)
 
 /* Burger icon menu for small screens */
 
 const toggleBurger = () => {
-	let a = document.getElementById('hiddenLinks');
-	if (a.style.display === "block") {
-		a.style.display = "none";
+	let a = document.getElementById('hiddenLinks')
+	if (a.style.display === 'block') {
+		a.style.display = 'none'
 	} else {
-		a.style.display = "block";
+		a.style.display = 'block'
 	}
-};
+}
 
 const closeAfter = () => {
-	let a = document.getElementById('hiddenLinks');
-	a.style.display = "none";
-};
+	let a = document.getElementById('hiddenLinks')
+	a.style.display = 'none'
+}
 
 /* Fade-in on main content */
 
-let initialSpin = false;
+let initialSpin = false
 
 window.onscroll = () => {
-	if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 350) {
-		document.getElementById('main-content').classList.add('fade-in');
+	if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 350) {
+		document.getElementById('main-content').classList.add('fade-in')
 		if (!initialSpin && !/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-			introPic.classList.add("spin-class");
-			initialSpin = true;
+			introPic.classList.add('spin-class')
+			initialSpin = true
 		}
 	}
-};
+}
 
 /* Change color */
 
 const getRandomNumber = () => {
-	const number = Math.floor(Math.random() * 360);
+	const number = Math.floor(Math.random() * 360)
 	if (number >= 210 && number <= 290) {
 		return getRandomNumber()
 	} else {
-		return number;
+		return number
 	}
-};
+}
 
 document.getElementById('change-color').addEventListener('click', () => {
-	document.documentElement.style.setProperty
-		('--interface-accent-color', `hsl(${getRandomNumber()}, 100%, 50%)`)
-});
+	document.documentElement.style.setProperty('--interface-accent-color', `hsl(${getRandomNumber()}, 100%, 50%)`)
+})
 
 /* Spin */
 
 const introPic = document.getElementById('intro-picture')
 
-introPic.addEventListener("click", function (e) {
-	e.preventDefault;
-	introPic.classList.remove("spin-class");
-	void introPic.offsetWidth;
-	introPic.classList.add("spin-class");
-}, false);
+introPic.addEventListener(
+	'click',
+	function (e) {
+		e.preventDefault
+		introPic.classList.remove('spin-class')
+		void introPic.offsetWidth
+		introPic.classList.add('spin-class')
+	},
+	false
+)
 
 const forceToNotSpin = () => {
-	introPic.classList.remove("spin-class");
-};
+	introPic.classList.remove('spin-class')
+}
 
 /* Project text fade on image hover */
 
@@ -202,7 +201,7 @@ const opacityIn = (number) => {
 		return
 	}
 
-	document.getElementsByClassName('overlay-text')[number].style.opacity = .2;
+	document.getElementsByClassName('overlay-text')[number].style.opacity = 0.2
 }
 
 const opacityOut = (number) => {
@@ -210,15 +209,15 @@ const opacityOut = (number) => {
 		return
 	}
 
-	document.getElementsByClassName('overlay-text')[number].style.opacity = 1;
+	document.getElementsByClassName('overlay-text')[number].style.opacity = 1
 }
 
 /* Make interface disappear for a brief moment */
 
 const makeInterfaceDisappear = () => {
-	document.getElementById('wrapper').style.opacity = 0;
+	document.getElementById('wrapper').style.opacity = 0
 
 	setTimeout(() => {
-		document.getElementById('wrapper').style.opacity = 1;
-	}, 300);
+		document.getElementById('wrapper').style.opacity = 1
+	}, 300)
 }
